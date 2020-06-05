311483e93d129cc90c3823288744e38b07812a5f
plkujaw <jawor.jakub@gmail.com>
2020-06-05 10:49:28 +0100

    add and pass test for two letter string

--- lib/search.rb
+++ lib/search.rb
@@ -1,3 +1,17 @@
 def replace(string)
-  '1'
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  string = string.downcase.chars
+  replaced = []
+
+  string.each_with_index do |letter, index|
+  p alphabet[index]
+  p string[index]
+        if string[index] == alphabet[index]
+          replaced << '1'
+        else
+          replaced << '0'
+        end
+
+    end
+    replaced.join('')
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -3,6 +3,12 @@ require 'search'
 describe '#replace' do
   it 'replaces letter a/A with 1 if it is present in a given string' do
     expect(replace('a')).to eq('1')
-    
+    expect(replace('A')).to eq('1')
   end
+
+  it 'replaces letter b/B with 1 if it is present in a given string' do
+    expect(replace('ab')).to eq('11')
+    expect(replace('Ab')).to eq('11')
+  end
+
 end
