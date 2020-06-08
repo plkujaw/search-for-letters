1d6478df93dc59e434a85eb09ab8bcc7a644aba6
plkujaw <jawor.jakub@gmail.com>
2020-06-08 11:29:17 +0100

    gave up for now

--- lib/search.rb
+++ lib/search.rb
@@ -1,4 +1,15 @@
 def replace(string)
   alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  string = string.downcase.chars
-  replaced = []
+  digits = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' ]
+p  string = string.downcase.gsub(/\W+/, '').chars.sort
+
+replaced = digits.map.with_index do |letter, index|
+  if alphabet[index] == string[index]
+    letter = '1'
+  else
+    letter = '0'
+end
+
+end
+  replaced.join
+end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,5 +1,8 @@
 require 'search'
 
 describe '#replace' do
-
+  it 'returns string of 10000000000000000000000000 for input string a' do
+    expect(replace('a')).to eq('10000000000000000000000000')
+    expect(replace('abdz')).to eq('11010000000000000000000001')
+  end
   end
